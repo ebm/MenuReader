79b14e881c48ec60bde8da205ec6cf38af59de29
Ethan Marantz <ebmarantz@gmail.com>
2026-04-06 00:24:07 -0400

    added comments

--- app/src/main/java/com/example/menureader/Front/MainActivity.java
+++ app/src/main/java/com/example/menureader/Front/MainActivity.java
@@ -6,6 +6,14 @@ import com.example.menureader.R;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    /**
+     * Gets called at the launch of MenuReader
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         LogHandler.m("In Main Activity");
